26fddfa1eff4160e846059764565386d44513459
stuartlogan82 <slogan@twilio.com>
2018-12-17 09:50:15 +0000

    added dequeue function

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request, current_app, jsonify
 from twilio.jwt.access_token import AccessToken
 from twilio.jwt.access_token.grants import SyncGrant
+from twilio.rest import Client
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -9,8 +10,9 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
 TWILIO_SYNC_SERVICE_SID = os.environ.get('TWILIO_SYNC_MAP_SERVICE_SID')
 TWILIO_API_KEY = os.environ.get('TWILIO_API_KEY')
-TWILIO_API_SECRET = os.environ.get('TWILIO_API_KEY_SECRET')
+TWILIO_API_SECRET = os.environ.get('TWILIO_API_SECRET')
 TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,6 +23,21 @@ def index():
 @app.route('/<number>', methods=['GET', 'POST'])
 def index_number(number):
     return render_template('index.html', googleMapsApiKey=GOOGLE_MAPS_API_KEY, identity=number)
+
+
+@app.route('/dequeue', methods=['POST'])
+def dequeue():
+    print(request.is_json)
+    content = request.get_json()
+    print(content)
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+    members = client.queues('QUb0d7407f6dc34f3cacc898794c164767').members.list()
+
+    for record in members:
+        print(record.call_sid)
+
+    return jsonify('JSON posted'), 200
 
 
 @app.route('/token')
